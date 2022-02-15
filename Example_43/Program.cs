696cd139e902bf4fadb1cd0a0a263eb0925fd4fa
LarisaZaripova <larik7lolik@gmail.com>
2022-02-15 21:02:37 +0300

    added example 43

---
+++ Example_43/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 43. Написать программу преобразования десятичного числа в двоичное
+/* Console.Clear();
+Console.Write("Введите двоичное число: ");
+int b = int.Parse(Console.ReadLine());
+string s = Convert.ToString(b, 2);
+Console.WriteLine(s); */

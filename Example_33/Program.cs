3fe6ad25cd788e628c4eea4b95eba1028e778e0d
LarisaZaripova <larik7lolik@gmail.com>
2022-02-15 14:53:24 +0300

    added new Example 33

--- Example_33/Program.cs
+++ Example_33/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 33: Задать массив из 12 элементов, заполненных
 //числами из [0,9]. Найти сумму положительных/отрицательных элементов массива.
-int[] array = new int[12];
+/* int[] array = new int[12];
 int i = 0;
 while (i < 12)
 {
@@ -26,5 +26,32 @@ while (a < 12)
 }   
 Console.WriteLine("Сумма положительных элементов массива равна " + sum);
 
-Console.WriteLine("Сумма отрицательных элементов массива равна " + summinus);
+Console.WriteLine("Сумма отрицательных элементов массива равна " + summinus); */
 
+Console.Clear();
+int[]array = new int[12];
+ void FillArray(int [] massiv)
+{
+    for(int i = 0; i < massiv.Length; i++)
+    {
+        massiv [i] = new Random().Next(-500, 501);
+    }
+}
+FillArray(array);
+ void PrintArray(int[]massiv)
+{
+    for(int i = 0; i < massiv.Length; i++)
+    {
+       Console.Write($"{massiv[i]} ");
+    }
+}
+PrintArray(array);
+int positive = 0;
+int negative = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0) positive += 1;
+    else negative += 1;
+}
+Console.WriteLine($"В заданном массиве {positive} положительных чисел и {negative} отрицательных");
+Console.WriteLine();

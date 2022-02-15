e62313931c02b25d3c31bb9b804e55838eb7de29
LarisaZaripova <larik7lolik@gmail.com>
2022-02-15 21:04:19 +0300

    added example 45

---
+++ Example_45/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа Фибоначи
+Console.Clear();
+Console.Write("Сколько чисел Ф вывести?  ");
+int count = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 1;
+int fibo = 0;
+Console.Write($"{a}  {b}  ");
+for(int i = 1; i <= count-2; i++)
+{
+   fibo = a + b;
+   a = b;
+   b = fibo;
+   Console.Write($"{fibo}  ");
+}
+Console.WriteLine();

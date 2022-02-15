c5ba637a91756a4f027a344ff8bf78d709088961
LarisaZaripova <larik7lolik@gmail.com>
2022-02-15 21:00:41 +0300

    added example 41

---
+++ Example_41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41. Выяснить являются ли три числа сторонами треугольника
+//Решение: неравенство треугольника: если есть три числа a,b,c,
+// то они являются сторонами треугольника если (a+b) > c и (b+c) > a и (a+c) >b
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = int.Parse(Console.ReadLine());
+if ((a+b) > c && (b+c) > a && (a+c) >b) Console.WriteLine(" Да, это стороны треугольника");
+else Console.Write("Нет, не являются");
+Console.WriteLine();
